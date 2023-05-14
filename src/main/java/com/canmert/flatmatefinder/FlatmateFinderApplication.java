616ac54d70522bd723beeb054a98792b95dd7f5a
Can Mert <canmert.dev@gmail.com>
2023-05-14 13:20:02 +0300

    base project

---
+++ src/main/java/com/canmert/flatmatefinder/FlatmateFinderApplication.java
@@ -0,0 +1,13 @@
+package com.canmert.flatmatefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatmateFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatmateFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/canmert/flatmatefinder/FlatmateFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canmert.flatmatefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatmateFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
